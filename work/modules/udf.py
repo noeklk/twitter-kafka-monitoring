3d1c6b642a2a69c1013237a8eda108c02043ffbb
Noé ABDEL KALEK <n.abdelkalek@ecole-ipssi.net>
2021-06-25 16:25:08 +0200

    removed unused data

--- work/modules/udf.py
+++
@@ -1,8 +0,0 @@
-from pyspark.sql.types import *
-from pyspark.sql.functions import udf
-
-
-@udf(returnType=StringType())
-def ms_to_display_string(ms):
-    m, s = divmod(ms / 1000, 60)
-    return f"{int(m)}min{int(s):0>2d}"
